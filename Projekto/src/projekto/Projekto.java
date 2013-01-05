537e0fa62a7e2809f13e1289e50ae12ff7493796
Julia <Julia@Julia-PC.speedport.ip>
2013-01-05 13:35:41 +0100

    LoginScreen, CreateAccount erstellt

---
+++ Projekto/src/projekto/Projekto.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekto;
+
+/**
+ *
+ * @author Julia
+ */
+public class Projekto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new LoginScreen();
+    }
+}
