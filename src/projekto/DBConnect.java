caf276e99ca54514fb7c49ba5474e1895f60d510
Tobias <Tobias@TobiasPC>
2013-04-02 23:27:47 +0200

    Login Authentifizierung - Überprüfung in der DB

--- src/projekto/DBConnect.java
+++ src/projekto/DBConnect.java
@@ -34,17 +34,13 @@ public class DBConnect {
     public void createUser(String email, String vorname, String nachname, String geburtsdatum, String strasse, String hausnr, String stadt, String plz, String pw) {
         Connection database;
         Statement statement;
-        //String nr = this.erzeugeNr();       //da erzeug ich eine zufällige 4stellige nummer
-        
-        //System.out.println(nr+" "+name+" "+adresse);
 
         try {
             Class.forName("org.postgresql.Driver").newInstance();
-            database = DriverManager.getConnection("jdbc:postgresql://localhost:5432/dbprojekt", "projekt", "geheim");
+            database = DriverManager.getConnection("jdbc:postgresql://localhost:5432/dbprojekt", "projekt", "geheim"); 
             statement = database.createStatement();
 
             Statement an = database.createStatement();
-            //an.executeUpdate("INSERT INTO \"Kino\" (\"KinoNr\", \"Name\", \"Adresse\") VALUES (" + nr + ", '" + name + "', '" + adresse + "')"); //diese zeile muss dementsprechend verändert werden (Benutzer)
             an.executeUpdate("INSERT INTO \"Benutzer\" (\"Name\", \"Vorname\", \"Email\", \"Passwort\", \"Strasse\", \"Hausnummer\", \"PLZ\", \"Ort\", \"Geburtsdatum\") " + 
                              "VALUES ('" + nachname + "', '" + vorname + "', '" + email + "', '" + pw + "', '" + strasse + "', '" + hausnr + "', '" + plz + "', '" + stadt + "', '" + geburtsdatum + "')");
             statement.close();
@@ -56,15 +52,33 @@ public class DBConnect {
         }
     }
     
-    /*
-    public static String erzeugeNr()
-	{
-	    int nr = (int) (Math.random()*10000);
-	    if (nr < 1) return "0000";
-	    if (nr < 10) return "000"+nr;
-	    if (nr < 100) return "00"+nr;
-	    if (nr < 1000) return "0"+nr;
-	    return ""+nr;
-	}
-        */
+    public String loginCheck(String user){
+        Connection database;
+        Statement statement;
+ 
+        String result = new String();
+        
+        try{
+            Class.forName("org.postgresql.Driver").newInstance();
+            database = DriverManager.getConnection("jdbc:postgresql://localhost:5432/dbprojekt", "projekt", "geheim"); 
+            statement = database.createStatement();
+            Statement an = database.createStatement();
+            
+            ResultSet rs = an.executeQuery("SELECT \"Passwort\" FROM \"Benutzer\" WHERE \"Email\" = '" + user + "'");
+            while( rs.next()){
+                result = rs.getString("Passwort");
+            }
+                
+            rs.close();
+            statement.close();
+            database.close();
+            
+            return result;
+        } catch (Exception ex) {
+            System.out.println("Keine Datenbankverbindung möglich: "
+                    + ex.getMessage());
+            return null;
+        }
+
+    }
 }
